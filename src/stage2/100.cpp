ad9695f44abf34e28520e054bf1388342e19ea6b
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2024-12-29 01:22:37 +0100

    Add stage2/100

---
+++ src/stage2/100.cpp
@@ -0,0 +1,119 @@
+#include "common.h"
+#include "scheduler.h"
+
+ParentSchedulerOwner::ParentSchedulerOwner() {
+    this->unk_18 = 0;
+    this->renderer2_ptr = NULL;
+}
+
+INCLUDE_ASM("nonmatchings/stage2/100", virt4__20ParentSchedulerOwner);
+
+INCLUDE_ASM("nonmatchings/stage2/100", virt5__20ParentSchedulerOwner);
+
+INCLUDE_ASM("nonmatchings/stage2/100", virt7__20ParentSchedulerOwner);
+
+INCLUDE_ASM("nonmatchings/stage2/100", virt8__20ParentSchedulerOwner);
+
+INCLUDE_ASM("nonmatchings/stage2/100", func_800470B4);
+
+INCLUDE_ASM("nonmatchings/stage2/100", func_800470C4);
+
+int ParentSchedulerOwner::virt11() {
+    return 1;
+} 
+
+int ParentSchedulerOwner::virt10() {
+    return 16;
+}
+
+int ParentSchedulerOwner::virt12() {
+    return 1;
+}
+
+int ParentSchedulerOwner::virt13() {
+    return 1;
+}
+
+int ParentSchedulerOwner::virt14() {
+    return 1;
+}
+
+int ParentSchedulerOwner::virt15() {
+    return 1;
+}
+
+int ParentSchedulerOwner::virt16() {
+    return 1;
+}
+
+int ParentSchedulerOwner::virt17() {
+    return 1;
+}
+
+int ParentSchedulerOwner::virt18() {
+    return 1;
+}
+
+int ParentSchedulerOwner::virt19() {
+    return 1;
+}
+
+int ParentSchedulerOwner::virt20() {
+    return 1;
+}
+
+int ParentSchedulerOwner::virt21() {
+    return 1;
+}
+
+int ParentSchedulerOwner::virt22() {
+    return 1;
+}
+
+int ParentSchedulerOwner::virt23() {
+    return 1;
+}
+
+int ParentSchedulerOwner::virt27() {
+    return 1;
+}
+
+int ParentSchedulerOwner::virt28() {
+    return 1; 
+}
+
+int ParentSchedulerOwner::virt24() {
+    return 0;
+}
+
+int ParentSchedulerOwner::virt26() {
+    return 1; 
+}
+
+int ParentSchedulerOwner::virt29() {
+    return 0;
+}
+
+int ParentSchedulerOwner::virt25() {
+    return 0;
+}
+
+void* ParentSchedulerOwner::func_800471C0() {
+    return this->renderer2_ptr;
+}
+
+int ParentSchedulerOwner::virt32() {
+    return 0;
+}
+
+int ParentSchedulerOwner::virt31() {
+    return 0;
+}
+
+int ParentSchedulerOwner::virt30() {
+    return 0;
+}
+ 
+INCLUDE_ASM("nonmatchings/stage2/100", func_800471E4);
+
+INCLUDE_RODATA("nonmatchings/stage2/100", D_8003C3C0); 
