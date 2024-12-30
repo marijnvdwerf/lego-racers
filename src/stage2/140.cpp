44bc6ef9b049b47b67a4e9423f1c43c491f25ffa
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2024-12-30 01:38:31 +0100

    Add stage2/140.cpp

---
+++ src/stage2/140.cpp
@@ -0,0 +1,64 @@
+#include "common.h"
+#include "scheduler.h"
+
+INCLUDE_ASM("nonmatchings/stage2/140", __26ParentParentSchedulerOwner);
+
+INCLUDE_ASM("nonmatchings/stage2/140", _._26ParentParentSchedulerOwner);
+
+INCLUDE_ASM("nonmatchings/stage2/140", virt4__26ParentParentSchedulerOwneriiii);
+
+INCLUDE_ASM("nonmatchings/stage2/140", virt7__26ParentParentSchedulerOwner);
+
+INCLUDE_ASM("nonmatchings/stage2/140", virt8__26ParentParentSchedulerOwneriiii);
+
+void ParentParentSchedulerOwner::virt2()
+{
+}
+
+void ParentParentSchedulerOwner::virt3()
+{
+}
+
+INCLUDE_ASM("nonmatchings/stage2/140", virt6__26ParentParentSchedulerOwner);
+
+INCLUDE_ASM("nonmatchings/stage2/140", virt5__26ParentParentSchedulerOwner);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C1B8);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C1C8);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C1D8);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C1E8);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C1F4);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C200);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C20C);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C218);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C224);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C230);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C23C);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C248);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C254);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C260);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C26C);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C278);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C284);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C290);
+
+INCLUDE_ASM("nonmatchings/stage2/140", func_8004C29C);
+
+INCLUDE_RODATA("nonmatchings/stage2/140", D_8003C7C0);
