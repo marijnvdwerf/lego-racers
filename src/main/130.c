fd0da0400e34d0ea30c7625648a45b698b4d023f
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2025-01-05 18:41:41 +0100

    clang-format

--- src/main/130.c
+++ src/main/130.c
@@ -1,6 +1,7 @@
 #include "common.h"
 
-void virt16__8Main_130(void) {
+void virt16__8Main_130(void)
+{
 }
 
 INCLUDE_ASM("nonmatchings/main/130", virt17__8Main_130);
@@ -9,7 +10,8 @@ INCLUDE_ASM("nonmatchings/main/130", virt19__8Main_130);
 
 INCLUDE_ASM("nonmatchings/main/130", virt20__8Main_130);
 
-void virt18__8Main_130(void) {
+void virt18__8Main_130(void)
+{
 }
 
 INCLUDE_ASM("nonmatchings/main/130", func_8000C30C);
